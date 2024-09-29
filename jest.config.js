20a99c2573212b560659afb060f56304a77e2c5d
staviasz <erickesc21@hotmail.com>
2024-09-29 09:27:18 -0300

    chore: test setup

---
+++ jest.config.js
@@ -0,0 +1,40 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require('next/jest');
+
+/** @type {import('jest').Config} */
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const config = {
+  preset: './jest-preset.js',
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  testMatch: ['<rootDir>/src/**/*.spec.{ts,tsx}'],
+  clearMocks: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/stories.tsx',
+    '!src/types/**/*',
+    '!src/app/layout.tsx',
+    '!node_modules/**',
+    '!src/app/api/**',
+    '!src/providers/sessionProvider.tsx',
+    '!src/app/**/*',
+  ],
+
+  moduleNameMapper: {
+    '^@public/(.*)$': '<rootDir>/public/$1',
+    '^@mocks/(.*)$': '<rootDir>/mocks/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+
+  // Add more setup options before each test is run
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(config);
