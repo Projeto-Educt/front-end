8a9056ff1a7fc2063857b6a7888ca39a5a994263
staviasz <erickesc21@hotmail.com>
2024-09-29 10:11:21 -0300

    chore: sass

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import path from 'path';
+
+const dirname = path.resolve('./src');
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(dirname, '**')],
+  },
+};
 
 export default nextConfig;
