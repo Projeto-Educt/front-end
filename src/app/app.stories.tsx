22f9598b6dcc842bdb48b35eedb0761aa889c1e5
staviasz <erickesc21@hotmail.com>
2024-09-29 09:49:38 -0300

    chore: stoorybook

---
+++ src/app/app.stories.tsx
@@ -0,0 +1,16 @@
+import Home from './page';
+
+export default {
+  title: 'Home',
+  component: Home,
+  args: {},
+  argTypes: {},
+};
+
+export const Template = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
